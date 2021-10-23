2897bb630ce04152da2df7034176cf92f3a0d47b
emre <emrky.2018@icloud.com>
2021-10-24 01:41:06 +0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let resultEl = document.getElementById("result");
+let numberEL = document.querySelectorAll(".number")
 
 
 let clickco = 0;  
@@ -29,8 +30,6 @@ function funCleaner() {
     clickco = 0;
     zeroFind = 0; 
     dotclick = 0; 
-   
-  
 }
 function numreversed(num) {
     let n = 0;
@@ -43,14 +42,15 @@ function numreversed(num) {
 function posnegfu(num1) {
     resultEl.innerHTML = -num1;
 }
-function divederFu() {
+
+ function divederFu() {
     if(zeroFind == 1){
         funcoun += 1;
         topl += toplO;
         if (funcoun == 1){sonuc1 += topl;}
         else{sonuc2 += topl; 
             let sonuc = sonuc1 / sonuc2;
-            resultEl.textContent = sonuc; }
+            resultEl.textContent = sonuc.toFixed(5); }
         funCleaner();
         toplO = 0;
   
@@ -61,11 +61,12 @@ function divederFu() {
         if (funcoun == 1){sonuc1 += newTopl1;}
         else{sonuc2 += newTopl1; 
         let sonuc = sonuc1 / sonuc2;
-        resultEl.textContent = sonuc; }
+        resultEl.textContent = sonuc.toFixed(5); }
         toplO = 0;
     }
   
 }
+
 function multipyFu() {
     if(zeroFind == 1){
         funcoun += 2;
@@ -94,6 +95,7 @@ function plusFu() {
     if(zeroFind == 1){
         funcoun += 4;
         topl += toplO;
+        console.log(topl)
         if (funcoun == 4){sonuc1 += topl;}
         else{sonuc2 += topl; 
             let sonuc = sonuc1 + sonuc2;
@@ -148,80 +150,20 @@ function dotFu() {
     toplO = 0;
 }
 
-function sevenFu(){
-    if(clickco == 0){resultEl.textContent = 7; topl += 7;}
-    else if(dotclick == 1){resultEl.textContent += 7; toplO += 7 * 10 ** (-clickco)}
-    else if(topl % 10 == 0){resultEl.textContent += 7; zeroFind = 1; topl = (topl * 10) + 7}
-    else {resultEl.textContent += 7; topl += (7 * 10 ** clickco);}
-    clickco++;
-  
-}
+numberEL.forEach(function(element){
+    element.onclick = function(){
+        if(clickco == 0){resultEl.textContent = Number(this.textContent); topl += Number(this.textContent);}
+        else if(dotclick == 1){resultEl.textContent += Number(this.textContent); toplO += Number(this.textContent) * 10 ** (-clickco);}
+        else if(topl % 10 == 0 || Number(this.textContent) != 0  ){resultEl.textContent += Number(this.textContent); zeroFind = 1; topl = (topl * 10) + Number(this.textContent);}
+        else if(topl % 10 == 0 || Number(this.textContent) == 0 ){resultEl.textContent += 0; zeroFind = 1; topl *= 10 ;console.log(topl)}
+        else if(Number(this.textContent) == 0 ){resultEl.textContent += 0;topl = numreversed(topl) ;  topl *= 10 ;}
+        else {resultEl.textContent += Number(this.textContent); topl += (Number(this.textContent) * 10 ** clickco);}
+        clickco++;
+    }
+});
 
-function eigthFu(){
-    if(clickco == 0){resultEl.textContent = 8; topl += 8;}
-    else if(dotclick == 1){resultEl.textContent += 8; toplO = toplO + 8 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 8; zeroFind = 1; topl = (topl * 10) + 8}
-    else {resultEl.textContent += 8; topl += (8 * 10 ** clickco);}
-    clickco++;
-}
 
-function nineFu(){
-    if(clickco == 0){resultEl.textContent = 9; topl += 9;}
-    else if(dotclick == 1){resultEl.textContent += 9; toplO = toplO + 9 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 9; zeroFind = 1; topl = (topl * 10) + 9}
-    else {resultEl.textContent += 9; topl += (9 * 10 ** clickco);}
-    clickco++;
-}
 
-function fourFu(){
-    if(clickco == 0){resultEl.textContent = 4; topl += 4;}
-    else if(dotclick == 1){resultEl.textContent += 4; toplO = toplO + 4 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 4; zeroFind = 1; topl = (topl * 10) + 4}
-    else {resultEl.textContent += 4; topl += (4 * 10 ** clickco);}
-    clickco++;
-}
-function fiveFu(){
-    if(clickco == 0){resultEl.textContent = 5; topl += 5;}
-    else if(dotclick == 1){resultEl.textContent += 5; toplO = toplO + 5 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 5; zeroFind = 1; topl = (topl * 10) + 5}
-    else {resultEl.textContent += 5; topl += (5 * 10 ** clickco);}
-    clickco++;
-}
-function sixFu(){
-    if(clickco == 0){resultEl.textContent = 6; topl += 6;}
-    else if(dotclick == 1){resultEl.textContent += 6; toplO = toplO + 6 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 6; zeroFind = 1; topl = (topl * 10) + 6}
-    else {resultEl.textContent += 6; topl += (6 * 10 ** clickco);}
-    clickco++;
-}
-function oneFu(){
-    if(clickco == 0){resultEl.textContent = 1; topl += 1;}
-    else if(dotclick == 1){resultEl.textContent += 1; toplO = toplO + 1 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 1; zeroFind =1; topl = (topl * 10) + 1}
-    else {resultEl.textContent += 1; topl += (1 * 10 ** clickco);}
-    clickco++;
-}
-function twoFu(){
-    if(clickco == 0){resultEl.textContent = 2; topl += 2;}
-    else if(dotclick == 1){resultEl.textContent += 2; toplO = toplO + 2 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 2; zeroFind = 1; topl = (topl * 10) + 2}
-    else {resultEl.textContent += 2; topl += (2 * 10 ** clickco);}
-    clickco++;
-}
-function threeFu(){
-    if(clickco == 0){resultEl.textContent = 3; topl += 3;}
-    else if(dotclick == 1){resultEl.textContent += 3; toplO = toplO + 3 * 10 ** -clickco }
-    else if(topl % 10 == 0){resultEl.textContent += 3; zeroFind = 1; topl = (topl * 10) + 3}
-    else {resultEl.textContent += 3; topl += (3 * 10 ** clickco);}
-    clickco++;
-}
-function zeroFu(){
-    if(clickco == 0){resultEl.textContent = 0; topl += 0;}
-    else if(topl % 10 == 0){resultEl.textContent += 0; zeroFind = 1; topl *= 10}
-    else {resultEl.textContent += 0;topl = numreversed(topl) ;  topl *= 10 ;}
-    console.log(topl)
-    clickco++;
-}
 
 function equalFu() {
     if (funcoun == 1 ){divederFu();}
